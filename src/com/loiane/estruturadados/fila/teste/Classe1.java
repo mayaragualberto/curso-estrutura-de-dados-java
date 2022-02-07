4d9b6cbb815473caa22f941e096ab3f80f7a8f0e
mayaragualberto <mayaragualberto@yahoo.com.br>
2022-02-07 09:35:57 -0300

    Segunda parte do curso

---
+++ src/com/loiane/estruturadados/fila/teste/Classe1.java
@@ -0,0 +1,17 @@
+package com.loiane.estruturadados.fila.teste;
+
+public class Classe1 implements Interface1{
+	
+	public void teste(){
+		
+	}
+
+	@Override
+	public void adiciona() {		
+	}
+
+	@Override
+	public void remove() {		
+	}
+
+}
